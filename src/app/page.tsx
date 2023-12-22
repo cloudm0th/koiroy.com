38ea4d5e1fab3bf189bd448ee31b7d6300800657
Jason Koerner <jreidko@gmail.com>
2023-12-21 19:26:23 -0600

    bump

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='h-[100dvh] width-full overflow-scroll hide-scrollbars flex flex-col justify-center items-center bg-fuchsia-950'>
-      <h1 className='koiroy-title'>Koiroy yo</h1>
-      <Image
-        className='m-4'
-        alt='koiroy logo'
-        src='/vercel.svg'
-        width={600}
-        height={400}
-      />
-      <Image
-        className='m-4'
-        alt='koiroy logo'
-        src='/next.svg'
-        width={300}
-        height={300}
-      />
-    </main>
+    <div className='center'>
+      <h1>
+        <span>koiroy</span>
+        <span>koiroy</span>
+        <span>koiroy</span>
+      </h1>
+    </div>
   )
 }
