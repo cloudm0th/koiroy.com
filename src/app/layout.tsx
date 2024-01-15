c3a0c6d09c0a79a6e19fe5af084afcf49d00198b
Jason Koerner <jreidko@gmail.com>
2024-01-15 11:53:59 -0600

    importing kalam to see if it works and it did - confirming that Kalnia is not in the next google fonts library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Kalam } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const kalam = Kalam({
+  subsets: ['latin'],
+  weight: '300',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={kalam.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className='h-screen flex flex-col justify-center items-center'>
-      <span className='text-green-400 font-bold text-5xl hover:text-red-500'>
-        koiroy
-      </span>
-      <span className='text-grey-400 transition-all duration-500 text-5xl hover:text-3xl'>
-        KOIROY
-      </span>
-      <span className='text-grey hover:text-fuchsia-400 transition duration-500 text-5xl'>
+      <span className='text-green-400 text-[100pt] hover:text-red-500 transition-all duration-500 text-5xl hover:text-[200pt]'>
         koiroy
       </span>
     </div>
