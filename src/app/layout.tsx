5fcb8276a761e72e6d2a1f4f989a0d6a23bdb644
Roy Koerner <royjkoerner@gmail.com>
2024-05-11 15:26:17 -0500

    pc is better than mac thanks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Lexend_Deca } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const lexend = Lexend_Deca({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
